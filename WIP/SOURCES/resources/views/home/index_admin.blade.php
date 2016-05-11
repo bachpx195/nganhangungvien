0acd5f4287520671bd9f0fe3029999369b596fa4
THANH LE VAN <thanhlv@bloomgoo.vn>
2016-05-12 00:54:50 +0700

    Update index_admin

---
+++ WIP/SOURCES/resources/views/home/index_admin.blade.php
@@ -0,0 +1,14 @@
+@extends('global_admin')
+<title>@lang('messages.site.title')</title>
+
+@section('content')
+
+<h3 class="page-title text-center">@lang('messages.site.title')</h3>
+
+<div class="row">
+	<div class="col-sm-12 text-center">
+
+	</div>
+</div>
+
+@endsection
