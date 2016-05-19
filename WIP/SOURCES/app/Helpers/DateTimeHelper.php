977a39a679011a58cd23bb210c3ed826a1e1c507
Cuong Nguyen <cuongcnb@gmail.com>
2016-05-19 23:58:35 +0700

    Search page

--- WIP/SOURCES/app/Helpers/DateTimeHelper.php
+++ WIP/SOURCES/app/Helpers/DateTimeHelper.php
@@ -5,9 +5,23 @@ use DateTime;
 
 class DateTimeHelper
 {
+	const SHORT_DATE_STANDARD = "d/m/Y";
+	
 	public static function convertDateFormat($dateString, $fromFormat, $toFormat){
 		$date = DateTime::createFromFormat($fromFormat, $dateString);
 
+		if(!$date){
+			return null;
+		}
+		
+		$newDateString = $date->format($toFormat);
+		
+		return $newDateString;
+	}
+	
+	public static function formatDate($dateString){
+		$date = DateTime::createFromFormat(SHORT_DATE_STANDARD, $dateString);
+		
 		if(!$date){
 			return null;
 		}
