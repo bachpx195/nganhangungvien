3ead038133ad06d4a854ac7cfc85ea03bcdbbb90
Cuong Nguyen <cuongcnb@gmail.com>
2016-05-20 23:05:42 +0700

    Delete components

--- WIP/SOURCES/app/Components/CandidateStatistics/views/index.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="box_alert_content w_100 font14 " id="box_alert_message">
-	<div class="col_msg_left">
-		<a href="#" class="btn btn-blue align-left"><span class="text_orange bold font16 pt_2">CẬP NHẬT HÔM NAY</span><span class="icon_24 icon_arrow_orange icon-24"></span></a>
-
-		<span style="margin-left: 20px;">Hôm qua có <span class="text_orange font18 bold">4,431</span> hồ sơ mới và <span class="text_orange font18 bold">10,287</span> hồ sơ cập nhật mới, trong tổng số <span class="text_orange font18 bold">610,976</span> hồ sơ đang tìm việc</span>
-	</div>
-</div>
--- WIP/SOURCES/resources/views/front/home/index.blade.php
+++ WIP/SOURCES/resources/views/front/home/index.blade.php
@@ -35,7 +35,13 @@
 		</div>
 	</div>
 	
-	{!! component()->uses('CandidateStatistics', ['args' => '1'])->render() !!}
+	<div class="box_alert_content w_100 font14 " id="box_alert_message">
+		<div class="col_msg_left">
+			<a href="#" class="btn btn-blue align-left"><span class="text_orange bold font16 pt_2">CẬP NHẬT HÔM NAY</span><span class="icon_24 icon_arrow_orange icon-24"></span></a>
+	
+			<span style="margin-left: 20px;">Hôm qua có <span class="text_orange font18 bold">4,431</span> hồ sơ mới và <span class="text_orange font18 bold">10,287</span> hồ sơ cập nhật mới, trong tổng số <span class="text_orange font18 bold">610,976</span> hồ sơ đang tìm việc</span>
+		</div>
+	</div>
 	
 	<div class="box_title_content w_100 font16 pos_relative mb_30 mt_16">
 		<h1 class="title_title_box pl_8 pr_8 uppercase bold text_blue">Cổng dành cho nhà tuyển dụng (610,976)</h1>
