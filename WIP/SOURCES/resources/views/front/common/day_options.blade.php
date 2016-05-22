97a7c33892c5e61f290e0f52a67abe877c4cb6fe
duchaviet <duc.ha@ntq-solution.com.vn>
2016-05-23 00:33:10 +0700

    Change day start and month start at that common options

--- WIP/SOURCES/resources/views/front/common/day_options.blade.php
+++ WIP/SOURCES/resources/views/front/common/day_options.blade.php
@@ -1,6 +1,6 @@
 {{ $selected = isset($selected) ? $selected : '' }}
 <option value="" @if($selected == '') selected @endif>Ngày</option>
-@for ($i = 0; $i <= 31; $i++)
+@for ($i = 1; $i <= 31; $i++)
     <option value="{{$i}}" @if($selected == $i) selected @endif>
         @if($i < 10) 0{{$i}} @else {{$i}} @endif
     </option>
