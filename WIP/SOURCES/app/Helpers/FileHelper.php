90abd397ec816e23d75ae9ae03a73affc836aed5
Bachpx <dongta195@gmail.com>
2016-05-31 13:17:23 +0700

    Them file FIlehelper và INewrepo

---
+++ WIP/SOURCES/app/Helpers/FileHelper.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace App\Helpers;
+
+class FileHelper {
+
+    /**
+     * Get a new file name by index
+     *
+     * @param int $index
+     * @return string
+     */
+    public static function getNewFileName($index)
+    {
+        if (empty($index)) {
+            return date('YmdHis') . '-' . substr(microtime(), 2,3)  . rand(0, 100);
+        } else {
+            return date('YmdHis') . '-' . substr(microtime(), 2,3) . $index . rand(0, 100);
+        }
+
+    }
+
+    /**
+     * Get the path of candidate image
+     */
+    public static function getCandidateImgPath()
+    {
+        return public_path() . '/candidate/image/';
+    }
+
+     public static function getNewsImgPath()
+    {
+        return public_path() . '/news/images/';
+    }
+}
