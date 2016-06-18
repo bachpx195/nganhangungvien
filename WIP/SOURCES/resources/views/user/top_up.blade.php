2d3d3b9655a781ad6014fd4e9954bbcfd323f579
duchaviet <duc.ha@ntq-solution.com.vn>
2016-06-18 20:51:58 +0700

    Change the name from VIETTEL to VIETEL

--- WIP/SOURCES/resources/views/user/top_up.blade.php
+++ WIP/SOURCES/resources/views/user/top_up.blade.php
@@ -27,7 +27,7 @@
             <div>
                 <a onclick="selectOptionMobile(this)" href="javascript:;"><i class="icon mobifone"></i><input type="radio" name="ncc" value="MOBI"/></a>
                 <a onclick="selectOptionMobile(this)" href="javascript:;"><i class="icon vinaphone"></i><input type="radio" name="ncc" value="VINA"/></a>
-                <a onclick="selectOptionMobile(this)" href="javascript:;"><i class="icon viettel"></i><input type="radio" name="ncc" value="VIETTEL"/></a>
+                <a onclick="selectOptionMobile(this)" href="javascript:;"><i class="icon viettel"></i><input type="radio" name="ncc" value="VIETEL"/></a>
                 <p><input  name="code_popup" id="code_popup" type="text" placeholder="Mã số thẻ"/></p>
                 <p><input name="seri_popup" id="seri_popup" type="text"  placeholder="Seri thẻ"name="txt_seri"/></p>
                 <p class="note">Mệnh giá tối thiểu 20.000 vnđ</p>
