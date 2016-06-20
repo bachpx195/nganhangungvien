077264de9ac485bd264551517c9a652c50af113a
DaiBX <daibx@fsoft.com.vn>
2016-06-20 21:07:18 +0700

    Change layout

--- WIP/SOURCES/resources/views/front/account/employer_profile.blade.php
+++ WIP/SOURCES/resources/views/front/account/employer_profile.blade.php
@@ -3,7 +3,7 @@
     <div class="container-fluid nopadding">
         <div class="pos_relative content_body">
             <div class="content_cols-right pt_16 pl_24 pb_24">
-                <h3 class="title_text_line mt_24 mb_8 ml_12 mr_12 uppercase">
+                <h3 class="title_text_line mb_8 ml_12 mr_12 uppercase">
                     <span class="fwb txt-color-363636 fs20">Thông tin tài khoản</span>
                 </h3>
                 <div class="box_tab bg_white box_shadow pt_12 pl_14 pr_14 pb_16 ml_12 mr_12">
@@ -40,101 +40,106 @@
                 </div>
             </div>
 
-            <h3 class="title_text_line mt_24 mb_8 ml_12 mr_12 uppercase">
-                <span class="fwb txt-color-363636 fs20">Thông tin công ty</span>
-            </h3>
-            <div class="box_tab bg_white box_shadow pt_12 pl_14 pr_14 pb_16 ml_12 mr_12">
-                <div class="w_100">
-                    <div class="box_info font14">
-                        <div class="pull-left w_100" id="employer_company_information_content">
-                            <div class="w_50 floatLeft">
-                                <div class="list-items mb_0">
-                                    <span class="fwb">Tên công ty: </span>{{ $employer->company_name }}
+
+            <div class="content_cols-right pl_24 pb_24">
+                <h3 class="title_text_line mb_8 ml_12 mr_12 uppercase">
+                    <span class="fwb txt-color-363636 fs20">Thông tin công ty</span>
+                </h3>
+                <div class="box_tab bg_white box_shadow pt_12 pl_14 pr_14 pb_16 ml_12 mr_12">
+                    <div class="w_100">
+                        <div class="box_info font14">
+                            <div class="pull-left w_100" id="employer_company_information_content">
+                                <div class="w_50 floatLeft">
+                                    <div class="list-items mb_0">
+                                        <span class="fwb">Tên công ty: </span>{{ $employer->company_name }}
+                                    </div>
+                                    <div class="list-items mb_0">
+                                        <span class="fwb">Quy mô công ty: </span>{{ $employer->companySize }}
+                                    </div>
+                                    <div class="list-items mb_0">
+                                        <span class="fwb">Địa chỉ công ty: </span>{{ $employer->company_address }}
+                                    </div>
+                                    <div class="list-items mb_0">
+                                        <span class="fwb">Tỉnh/thành phố: </span>{{ $employer->provinceName }}
+                                    </div>
+                                    <div class="list-items mb_0">
+                                        <span class="fwb">Điện thoại cố định: </span>{{ $employer->phone }}
+                                    </div>
+                                    <div class="list-items mb_0">
+                                        <span class="fwb">Website: </span>{{ $employer->website }}
+                                    </div>
                                 </div>
-                                <div class="list-items mb_0">
-                                    <span class="fwb">Quy mô công ty: </span>{{ $employer->companySize }}
-                                </div>
-                                <div class="list-items mb_0">
-                                    <span class="fwb">Địa chỉ công ty: </span>{{ $employer->company_address }}
-                                </div>
-                                <div class="list-items mb_0">
-                                    <span class="fwb">Tỉnh/thành phố: </span>{{ $employer->provinceName }}
-                                </div>
-                                <div class="list-items mb_0">
-                                    <span class="fwb">Điện thoại cố định: </span>{{ $employer->phone }}
-                                </div>
-                                <div class="list-items mb_0">
-                                    <span class="fwb">Website: </span>{{ $employer->website }}
-                                </div>
-                            </div>
-                            <div class="w_33 floatLeft">
-                                <div class="box_info font14">
-                                    <div class="w_100">
-                                        <div class="w_33 floatLeft"><strong>Ảnh đại diện: </strong></div>
-                                        <div class="fileinput-new custom-thumbnail custom-upload-image floatLeft">
-                                            @if(empty($employer->image))
-                                                <img src="{{ URL::asset('assets/image/default.png') }}" height="130"
-                                                    width="170" alt=""/>
-                                            @else
-                                                <img src="{{ URL::to('/') . $employer->image }}" height="130"
-                                                     width="170" alt=""/>
-                                            @endif
+                                <div class="w_33 floatLeft">
+                                    <div class="box_info font14">
+                                        <div class="w_100">
+                                            <div class="w_33 floatLeft"><strong>Ảnh đại diện: </strong></div>
+                                            <div class="fileinput-new custom-thumbnail custom-upload-image floatLeft">
+                                                @if(empty($employer->image))
+                                                    <img src="{{ URL::asset('assets/image/default.png') }}" height="130"
+                                                         width="170" alt=""/>
+                                                @else
+                                                    <img src="{{ URL::to('/') . $employer->image }}" height="130"
+                                                         width="170" alt=""/>
+                                                @endif
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="w_100 floatLeft">
-                                <div class="list-items mb_0">
-                                    <span class="fwb">Giới thiệu về công ty:</span>
+                                <div class="w_100 floatLeft">
+                                    <div class="list-items mb_0">
+                                        <span class="fwb">Giới thiệu về công ty:</span>
+                                    </div>
+                                    <div class="list-items mb_0">
+                                        {{ $employer->company_description }}
+                                    </div>
                                 </div>
-                                <div class="list-items mb_0">
-                                    {{ $employer->company_description }}
-                                </div>
+                                <div class="clearfix"></div>
                             </div>
-                            <div class="clearfix"></div>
-                        </div>
-                        <div class="list-items fc-btn-edit">
-                            <button id="btnChangeCompanyInfo"
-                                    class="btn btn-white no-shadow btn-xs font14 pr_12 pl_6 mt_10 show_popup_s33_4">
+                            <div class="list-items fc-btn-edit">
+                                <button id="btnChangeCompanyInfo"
+                                        class="btn btn-white no-shadow btn-xs font14 pr_12 pl_6 mt_10 show_popup_s33_4">
                             <span class="line-icon text_grey3">
                                 <i class="icon-24 icon_24 icon-pencil pos_absolute"></i> <span
                                         class="pl_28"> Chỉnh sửa</span>
                             </span>
-                            </button>
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
 
-            <h3 class="title_text_line mt_24 mb_8 ml_12 mr_12 uppercase">
-                <span class="fwb txt-color-363636 fs20">Thông tin chủ tài khoản</span>
-            </h3>
-            <div class="box_tab bg_white box_shadow pt_12 pl_14 pr_14 pb_16 ml_12 mr_12">
-                <div class="w_100">
-                    <div class="box_info chu_tai_khoan font14">
-                        <div class="pull-left w_100" id="employer_contact_person_content">
-                            <div class="list-items mb_0">
-                                <span class="fwb">Tên người liên hệ: </span><span
-                                        class="name"> {{ $employer->contact_person }}</span>
+            <div class="content_cols-right pl_24 pb_24">
+                <h3 class="title_text_line mb_8 ml_12 mr_12 uppercase">
+                    <span class="fwb txt-color-363636 fs20">Thông tin chủ tài khoản</span>
+                </h3>
+                <div class="box_tab bg_white box_shadow pt_12 pl_14 pr_14 pb_16 ml_12 mr_12">
+                    <div class="w_100">
+                        <div class="box_info chu_tai_khoan font14">
+                            <div class="pull-left w_100" id="employer_contact_person_content">
+                                <div class="list-items mb_0">
+                                    <span class="fwb">Tên người liên hệ: </span><span
+                                            class="name"> {{ $employer->contact_person }}</span>
+                                </div>
+                                <div class="list-items mb_0">
+                                    <span class="fwb">Số điện thoại liên hệ: </span><span
+                                            class="tel"> {{ $employer->contact_phone }}</span>
+                                </div>
+                                <div class="list-items mb_0">
+                                    <span class="fwb">Email liên hệ: </span><span
+                                            class="email"> {{ $employer->contact_email }}</span>
+                                </div>
+                                <div class="clearfix"></div>
                             </div>
-                            <div class="list-items mb_0">
-                                <span class="fwb">Số điện thoại liên hệ: </span><span
-                                        class="tel"> {{ $employer->contact_phone }}</span>
-                            </div>
-                            <div class="list-items mb_0">
-                                <span class="fwb">Email liên hệ: </span><span
-                                        class="email"> {{ $employer->contact_email }}</span>
-                            </div>
-                            <div class="clearfix"></div>
-                        </div>
-                        <div class="list-items fc-btn-edit">
-                            <button id="btnChangeContactPerson"
-                                    class="btn btn-white no-shadow btn-xs font14 pr_12 pl_6 mt_10 show_popup_s33_5">
+                            <div class="list-items fc-btn-edit">
+                                <button id="btnChangeContactPerson"
+                                        class="btn btn-white no-shadow btn-xs font14 pr_12 pl_6 mt_10 show_popup_s33_5">
                             <span class="line-icon text_grey3">
                                 <i class="icon-24 icon_24 icon-pencil pos_absolute"></i> <span
                                         class="pl_28"> Chỉnh sửa</span>
                             </span>
-                            </button>
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
