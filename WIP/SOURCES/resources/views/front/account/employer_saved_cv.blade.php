aa29e048ae049a22a185dbab105a7386a619af56
THANH LE VAN <thanhlv@bloomgoo.vn>
2016-06-23 23:51:17 +0700

    Update style Quản lý tài khoản & Lịch sử & Hồ sơ đã lưu

--- WIP/SOURCES/resources/views/front/account/employer_saved_cv.blade.php
+++ WIP/SOURCES/resources/views/front/account/employer_saved_cv.blade.php
@@ -5,67 +5,70 @@
     }
 </style>
 @section('content')
-    <div class="container-fluid nopadding">
-        <div class="content_body">
-            <div class="content_cols-right pt_16 pl_24 pb_24">
-                <h3 class="title_text_line mt_24 mb_8 ml_12 mr_12 uppercase">
-                    <span class="fwb txt-color-363636 fs20">Hồ sơ tìm việc đã lưu</span>
-                </h3>
-                <div class="box_tab bg_white box_shadow pt_12 pl_14 pr_14 pb_16 ml_12 mr_12">
-                    <div class="w_100">
-                        <div class="box_info font14">
-                            <div class="tablehsut">
-                                <div id="list_hs">
-                                    <input id="hdfStart" type="hidden" name="start" value="{{ $start }}">
-                                    <input id="hdfLimit" type="hidden" name="limit" value="{{ $limit }}">
-                                    <table>
-                                        <thead>
-                                        <tr style="background: rgb(245, 245, 245);">
-                                            <th style="width: 45px;">STT</th>
-                                            <th>Tên hồ sơ ứng viên</th>
-                                            <th style="width: 20%;">Thời gian lưu</th>
-                                        </tr>
-                                        </thead>
-                                        <tbody id="table_employer_saved_cv">
-                                        @if(count($saveCvs) > 0)
-                                            @foreach($saveCvs as $index=>$item)
-                                                <tr>
-                                                    <td style="width: 45px;">
-                                                        {{ $index + 1 }}
-                                                    </td>
-                                                    <td>
-                                                        <a href="{{route('candidate.profile', ['slug' => StringHelper::uri($item->cv_title), 'id' => $item->candidateId])}}">{{ $item->candidateName }}</a> ({{ $item->cv_title }})
-                                                    </td>
-                                                    <td style="width: 20%;">
-                                                        {{ $item->created_at }}
-                                                    </td>
-                                                </tr>
-                                            @endforeach
-                                        @else
-                                            <tr>
-                                                <td colspan="5" class="center">Bạn chưa lưu hồ sơ nào</td>
-                                            </tr>
-                                        @endif
-                                        </tbody>
-                                    </table>
-                                </div>
+
+    @if (Auth::check() && Auth::user()->user_type == 'employer')
+        <div class="content_cols-right" style="padding-left:232px;">
+    @else
+        <div class="content_cols-right">
+    @endif
+    <div class="content_cols-right pt_16 pb_24">
+        <h3 class="title_text_line mt_24 mb_8 uppercase">
+            <span class="fwb txt-color-363636 fs20">Hồ sơ tìm việc đã lưu</span>
+        </h3>
+        <div class="box_tab bg_white box_shadow pt_12 pl_14 pr_14 pb_16">
+            <div class="w_100">
+                <div class="box_info font14">
+                    <div class="tablehsut">
+                        <div id="list_hs">
+                            <input id="hdfStart" type="hidden" name="start" value="{{ $start }}">
+                            <input id="hdfLimit" type="hidden" name="limit" value="{{ $limit }}">
+                            <table>
+                                <thead>
+                                <tr style="background: rgb(245, 245, 245);">
+                                    <th style="width: 45px;">STT</th>
+                                    <th>Tên hồ sơ ứng viên</th>
+                                    <th style="width: 20%;">Thời gian lưu</th>
+                                </tr>
+                                </thead>
+                                <tbody id="table_employer_saved_cv">
                                 @if(count($saveCvs) > 0)
-                                    <div class="transaction_table_bottom center button_load_more">
-                                        <button id="btnShowMoreSavedCv"
-                                                class="btn btn-white no-shadow btn-xs font14 pr_12 pl_6 mt_10 show_popup_s33_4">
-                                        <span class="line-icon">
-                                            <i class="icon-24 icon_24 icon-pencil pos_absolute"></i> <span
-                                                    class="pl_28"> Hiển thị thêm</span>
-                                        </span>
-                                        </button>
-                                    </div>
+                                    @foreach($saveCvs as $index=>$item)
+                                        <tr>
+                                            <td style="width: 45px;">
+                                                {{ $index + 1 }}
+                                            </td>
+                                            <td>
+                                                <a href="{{route('candidate.profile', ['slug' => StringHelper::uri($item->cv_title), 'id' => $item->candidateId])}}">{{ $item->candidateName }}</a> ({{ $item->cv_title }})
+                                            </td>
+                                            <td style="width: 20%;">
+                                                {{ $item->created_at }}
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                @else
+                                    <tr>
+                                        <td colspan="5" class="center">Bạn chưa lưu hồ sơ nào</td>
+                                    </tr>
                                 @endif
-                            </div>
+                                </tbody>
+                            </table>
                         </div>
+                        @if(count($saveCvs) > 0)
+                            <div class="transaction_table_bottom center button_load_more">
+                                <button id="btnShowMoreSavedCv"
+                                        class="btn btn-white no-shadow btn-xs font14 pr_12 pl_6 mt_10 show_popup_s33_4">
+                                <span class="line-icon">
+                                    <i class="icon-24 icon_24 icon-pencil pos_absolute"></i> <span
+                                            class="pl_28"> Hiển thị thêm</span>
+                                </span>
+                                </button>
+                            </div>
+                        @endif
                     </div>
                 </div>
             </div>
         </div>
+    </div>
     </div>
     <div hidden="hidden">
         @include('front.account.template.template_employer_saved_cv')
