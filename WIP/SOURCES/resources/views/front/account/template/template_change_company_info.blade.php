92bccf2e690462a7919d2f409f60fa10dcedbae8
DaiBX <daibx@fsoft.com.vn>
2016-06-26 23:28:20 +0700

    TASK119: Fix bug employer detail

--- WIP/SOURCES/resources/views/front/account/template/template_change_company_info.blade.php
+++ WIP/SOURCES/resources/views/front/account/template/template_change_company_info.blade.php
@@ -38,25 +38,4 @@
             @{{ company_description }}
         </div>
     </div>
-    <div class="clearfix"></div>
-
-    <div class="list-items mb_0">
-        <span class="fwb">Địa chỉ công ty: </span>@{{ company_address }}
-    </div>
-    <div class="list-items mb_0">
-        <span class="fwb">Tỉnh/thành phố: </span>@{{ provinceName }}
-    </div>
-    <div class="list-items mb_0">
-        <span class="fwb">Điện thoại cố định: </span>@{{ phone }}
-    </div>
-    <div class="list-items mb_0">
-        <span class="fwb">Website: </span>@{{ website }}
-    </div>
-    <div class="list-items mb_0">
-        <span class="fwb">Giới thiệu về công ty:</span>
-    </div>
-    <div class="list-items mb_0">
-        @{{ company_description }}
-    </div>
-    <div class="clearfix"></div>
 </div>
