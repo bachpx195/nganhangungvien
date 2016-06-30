b6c1708be264f996d58753433d29fccfafe7b61e
duchaviet <duc.ha@ntq-solution.com.vn>
2016-06-30 07:43:55 +0700

    Revert test code

--- WIP/SOURCES/app/Libs/BaoKim/Card.php
+++ WIP/SOURCES/app/Libs/BaoKim/Card.php
@@ -94,7 +94,6 @@ class Card {
 
             return array('success' => true, 'amount' => $token);
         } else {
-            return array('success' => true, 'amount' => 50);
             return array('success' => false, 'err' => 'Status Code:' . $status . '.'. 'Error:'.$result['errorMessage']);
         }
     }
