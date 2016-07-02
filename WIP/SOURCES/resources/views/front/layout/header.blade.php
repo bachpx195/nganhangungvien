d1b92fca5f259ddd0b3ad04ffc2350cd5b270f97
THANH LE VAN <thanhlv@bloomgoo.vn>
2016-07-03 03:36:43 +0700

    Update menu "Nha Tuyen Dung"

--- WIP/SOURCES/resources/views/front/layout/header.blade.php
+++ WIP/SOURCES/resources/views/front/layout/header.blade.php
@@ -88,13 +88,43 @@
 				</div>
 			</div>
 			@endif
+
 			<div class="menu_cong_header1 floatRight">
 				<?php $activeHeaderMenu = isset($activeHeaderMenu) ? $activeHeaderMenu : 'employer'?>
-				<a class="bt_head_cong floatLeft {{($activeHeaderMenu == 'employer') ? 'active' : ''}}" href="{{route('employer.register')}}"
+
+					<div class="main-menu bt_head_cong floatLeft">
+						<ul class="nopadding nomargin">
+							<li class="pos_relative dang_nhap" style="list-style: none">
+								<a href="javascript:;" data-toggle="tooltip"
+								   data-placement="bottom">
+									<div class="icn_cong_head_ntd mt_20"></div>
+									<div class="title_cong_menu w150">NHÀ TUYỂN DỤNG</div>
+								</a>
+
+								<div class="dang_nhap popup_down">
+									<div class="header-user-info"></div>
+									<div class="user-info w150 bg_white box_shadow">
+										<div class="pl_16 pr_16 pt_16 pb_14 bg_white user_box">
+											<a href="javascript:;"
+											   class="member_login btn font16 btn-blue btn-block btn-lg mb_16 pb_9">Đăng
+												nhập</a>
+											<a href="{{route('employer.register')}}"
+											   class="member_register btn font16 btn-pink btn-block btn-lg pb_9">Đăng
+												ký</a>
+										</div>
+									</div>
+								</div>
+							</li>
+						</ul>
+					</div>
+
+				{{--<a class="bt_head_cong floatLeft {{($activeHeaderMenu == 'employer') ? 'active' : ''}}" href="{{route('employer.register')}}"
 					data-toggle="tooltip" data-placement="bottom" title="">
 					<div class="icn_cong_head_ntd mt_20"></div>
 					<div class="title_cong_menu w_100">NHÀ TUYỂN DỤNG</div>
-				</a>
+
+				</a>--}}
+
 				<a class="bt_head_cong floatLeft {{($activeHeaderMenu == 'candidate') ? 'active' : ''}}"
 				   	href="{{route('candidate.form')}}"
 					data-toggle="tooltip" data-placement="bottom" title="">
