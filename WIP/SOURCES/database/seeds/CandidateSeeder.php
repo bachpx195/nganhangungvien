7a0be5a2c69c88cc88c9ed59ed373dd5602c8f34
Cuong Nguyen <cuongcnb@gmail.com>
2016-07-03 23:08:23 +0700

    #update seed

--- WIP/SOURCES/database/seeds/CandidateSeeder.php
+++ WIP/SOURCES/database/seeds/CandidateSeeder.php
@@ -8,9 +8,11 @@ class CandidateSeeder extends Seeder {
 	public function run() 
 	{
 		DB::table('candidate')->delete();
-		
+
+		//Chỉ xóa, ko insert vì dữ liệu ko đủ
+
 		// Manager
-		Candidate::insert([
+		/*Candidate::insert([
 			'Full_name' => 'Liêu Anh Khoa',
 			'updated_at' => '11/05/2016',
 			'Expect_salary' => '1',
@@ -143,7 +145,7 @@ class CandidateSeeder extends Seeder {
 			'Expect_salary' => '4',
 			'email' => '19@mail.com',
 			'Experience_years' => '2',
-		]);
+		]);*/
 	}
 	
 }
