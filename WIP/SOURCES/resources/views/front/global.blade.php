7271e1327d118dafe3fdb7da36d0495e6be2f74c
THANH LE VAN <thanhlv@bloomgoo.vn>
2016-07-03 11:02:21 +0700

    Update style "Tai Lieu Quan Tri"

--- WIP/SOURCES/resources/views/front/global.blade.php
+++ WIP/SOURCES/resources/views/front/global.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
-    <title>NGAN HANG UNG VIEN</title>
+    <title>NGÂN HÀNG ỨNG VIÊN</title>
 
     <!-- css all -->
     <link rel="stylesheet" href="{{ asset('/assets/default/css/normalize.min.css') }}" property='stylesheet'
