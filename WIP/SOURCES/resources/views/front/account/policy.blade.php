b3c4801a54dd3dd589a340f6d4a0d93b8be26b38
duchaviet <duc.ha@ntq-solution.com.vn>
2016-07-17 23:41:03 +0700

    Add policy content

---
+++ WIP/SOURCES/resources/views/front/account/policy.blade.php
@@ -0,0 +1,62 @@
+<div class="block-policy">
+    <div class="content">
+        @if ($policy !== '')
+            {{$policy}}
+        @else 
+            <p>Nganhangungvien.com là chuyên trang thông tin cho người tìm việc và nhà tuyển dụng. Chúng tôi không hoạt động về dịch vụ Giới thiệu việc làm. Mọi thông tin do nhà tuyển dụng và người tìm việc tự đăng tải tại website. Chúng tôi đã cố gắng kiểm tra thông tin nhà tuyển dụng trước khi kích hoạt nội dung, Nếu phát hiện thông tin xấu hoặc doanh nghiệp đăng tải thông tin trục lợi người lao động (thu tiền hồ sơ, thu tiền đồng phục, đóng phí ... Các kiểu để người lao động phải nộp tiền) vui lòng báo ngay cho người quản trị website chúng tôi hoặc Sở Lao Động Thương Binh Xã Hội nơi địa bàn quản lý của doanh nghiệp đó về hành vi trục lợi người lao động hoặc các dịch vụ việc làm không phép. <strong>Hãy cùng nhau xây dựng niềm tin trong một thế giới đầy ấp thông tin nhưng hơi thiếu sự tin tưởng</strong>. Cam kết Miễn Phí Vĩnh Viễn <strong>đăng tin</strong> tuyển dụng và tìm việc.<br>
+                <br>
+                Trước khi sử dụng Web Nganhangungvien.com xin vui lòng đọc trang này cẩn thận. Nếu bạn không đồng ý với những thỏa thuận và điều kiện đưa ra từ Nganhangungvien.com, xin vui lòng ngưng ngay việc truy cập và sử dụng trang Nganhangungvien.com. Nếu bạn truy cập hoặc sử dụng Web Nganhangungvien.com xem như bạn đã chấp nhận những thỏa thuận, điều kiện và những điều khoản ràng buộc của Nganhangungvien.com đưa ra.<br>
+                <br>
+                Nganhangungvien.com có thể thay đổi thoả thuận sử dụng theo từng thời kỳ để phù hợp với Quy Định Pháp Luật hiện hành mà không cần thông báo trước. Bạn nên vào trang này định kỳ để xem lại những thỏa thuận và điều kiện bởi những điều kiện này sẽ ràng buôc bạn và Nganhangungvien.com. Những thuật ngữ Bạn và Người sử dụng được dùng ở đây đề cập đến tất cả những cá nhân hoặc những tổ chức truy cập vào trang web này với bất cứ lý do nào. Thoả thuận sử dụng dịch vụ gồm những điều khoản sau:<br>
+                <br>
+                •&nbsp;&nbsp; Tất cả nội dung trang Web Nganhangungvien.com (Như văn bản, biểu tượng, hình ảnh, hình tượng và nội dung) thuộc tài sản sở hữu trí tuệ được Pháp Luật Việt Nam bảo vệ. Bạn không được quyền dùng bất cứ biện pháp kỹ thuật nào để sao chép hoặc sửa chữa bất cứ nội dung nào tại Web Nganhangungvien.com mà không được sự đồng ý của Nganhangungvien.com.<br>
+                <br>
+                •&nbsp;&nbsp; Bạn không được gửi hoặc chuyển các thông tin bất hợp pháp, vi phạm thuần phong mỹ tục, đe doạ, lạm dụng, bôi nhọ, nói xấu, khiêu dâm, phi thẩm mỹ, xúc phạm hoặc bất kỳ loại thông tin không đứng đắn nào, bao gồm những việc truyền bá tin tức góp phần hay khuyến khích hành vi phạm tội, gây ra trách nhiệm pháp lý dân sự và hình sự.<br>
+                <br>
+                •&nbsp;&nbsp; Bạn không được đăng tin mang tính chất quảng cáo, quảng bá hình ảnh, sản phẩm, dịch vụ...</p>
+
+                <p>• &nbsp; Bạn không được đăng nhiều tin có cùng một nội dung hoặc có nội dung tương tự trong thông tin tuyển dụng trên web Nganhangungvien.com.</p>
+
+                <p>•&nbsp; Bạn không được đăng quá 2 tin đăng có nội dung tương tự như nhau trong cùng ngành nghề, nếu có thì phải thông báo cho Nganhangungvien.com và được Nganhangungvien.com chấp nhận bằng văn bản.<br>
+                <br>
+                •&nbsp; Web Nganhangungvien.com hoàn toàn không chịu trách nhiệm khi bạn gửi hay chuyển các thông tin, phần mềm, hoặc các tài liệu khác bất kỳ vi phạm hoặc xâm phạm tác quyền của những người khác, trong đó bao gồm cả tài liệu xâm phạm đến quyền&nbsp; riêng tư hoặc công khai tài liệu được bảo vệ bản quyền, tên thương mại hoặc quyền sở hữu khác, các sản phẩm phát sinh mà không được sự cho phép của người chủ sở hữu hoặc người có quyền hợp pháp. Hoặc vi phạm một quy tắc, chính sách hay hướng dẫn sử dụng nào của nhà cung cấp dịch vụ Internet và Luật Pháp Việt Nam.<br>
+                <br>
+                •&nbsp;&nbsp;&nbsp; Nếu bạn tin rằng sản phẩm đăng ký bản quyền của bạn đã được tải lên, đăng tải hay sao chép lên trang web Nganhangungvien.com&nbsp; và được sử dụng trên trang web Nganhangungvien.com mà&nbsp; xâm phạm bản quyền của bạn, vui lòng thông báo cho Nganhangungvien.com bằng cách cung cấp đại lý bản quyền chỉ định với những thông tin liên quan đến việc được cung cấp bản quyền của bạn. Nganhangungvien.com sẽ ngưng toàn bộ việc đăng tải, sử dụng sản phẩm bản quyền của bạn khi đã chứng minh tính xác thực mà bạn cung cấp cho Nganhangungvien.com.<br>
+                <br>
+                •&nbsp;&nbsp;&nbsp; Bạn không được quyền tuyên bố hay phát ngôn cho một doanh nghiệp, hiệp hội hay tổ chức nào khác mà bạn không được uỷ quyền tuyên bố mối liên hệ đó.<br>
+                <br>
+                •&nbsp;&nbsp;&nbsp; Bạn chịu trách nhiệm lưu giữ bảo mật thông tin và mật khẩu của bạn. Bạn sẽ chịu trách nhiệm với tất cả những sử dụng hoặc đăng ký do tài khoản của bạn tạo ra. Khi bị thất lạc hoặc mất thông tin cá nhân, bạn phải liên hệ ngay với Nganhangungvien.com để được hỗ trợ.<br>
+                <br>
+                •&nbsp;&nbsp;&nbsp; Khi sử dụng tiện ích của Nganhangungvien.com để đăng thông tin, bạn có quyền sửa chữa các thông tin đã đăng trong phạm vi tài khoản, thông tin của mình. Tuy nhiên, đối với các tin đăng tuyển, việc sửa chữa chỉ có giá trị và được đăng trên website sau khi ban biên tập web Nganhangungvien.com kiểm tra và kích hoạt trong thời gian bất kỳ mà không cần thông báo trước.<br>
+                <br>
+                •&nbsp;&nbsp;&nbsp; Công ty sẽ không tiết lộ cho bất cứ bên thứ ba tên, địa chỉ, địa chỉ email hay số điện thoại của bạn mà không có sự đồng ý ưu tiên của bạn, ngoại trừ sự cần thiết nhất định hay thích hợp để tuân theo những luật pháp hiện hành.<br>
+                <br>
+                •&nbsp;&nbsp;&nbsp; Quyền duy nhất của bạn là khi không thỏa mãn chính sách, yêu cầu, thỏa thuận và hoạt động thực tiễn của Nganhangungvien.com thì bạn vui lòng chấm dứt mọi truy cập và giao dịch của bạn với Nganhangungvien.com ngay lập tức. Nganhangungvien.com sẽ chấm dứt hoặc tạm ngưng hoạt động của bạn mà không cần bồi thường với bạn dưới hình thức nào.<br>
+                <br>
+                •&nbsp;&nbsp;&nbsp; Nganhangungvien.com không thể xác định được người sử dụng thừa nhận thực về chính mình, Nganhangungvien.com không tham gia giao tiếp hoặc kiểm soát hành vi người sử dụng. Cho nên bạn không thể đòi hỏi bất cứ thiệt hại nào với Nganhangungvien.com liên quan đến người sử dụng khác.<br>
+                <br>
+                •&nbsp;&nbsp;&nbsp; Nganhangungvien.com không phải là Nhà Tuyển Dụng hoặc đại diện cho bất cứ Nhà Tuyển Dụng nào khác. Nganhangungvien.com sẽ không chịu trách nhiệm với bất cứ quyết định nào trong công việc lựa chọn của bạn với mọi lý do.<br>
+                <br>
+                •&nbsp;&nbsp;&nbsp; Nganhangungvien.com không chịu trách nhiệm đối với bất cứ đường liên kết đến các website nào khác từ Nganhangungvien.com. Những liên kết này chỉ mang tính chất cung cấp như một tiện ích mà Nganhangungvien.com mang lại cho bạn. Nganhangungvien.com không đại diện cho liên kết thứ ba và không đảm bảo rằng các nội dung, tính chính xác và những thiệt hại của những liên kết thứ ba mang lại cho bạn. Nếu bạn cố gắng liên kết thì đó là sự lựa chọn của bạn.<br>
+                <br>
+                •&nbsp;&nbsp;&nbsp; Nganhangungvien.com không chứng nhận và đảm bảo rằng Nganhangungvien.com hay những liên kết sẽ hoạt động mà không có lỗi nào và máy chủ của nó là hoàn toàn không nhiễm virus hay những phần mềm có hại khác. Nếu bạn sử dụng dịch vụ&nbsp; Nganhangungvien.com dẫn đến nguy hại thay thế kỹ thuật hay dữ liệu riêng của bạn thì Nganhangungvien.com sẽ hoàn toàn không chịu trách nhiệm với những giá trị này.<br>
+                <br>
+                •&nbsp;&nbsp;&nbsp; <strong>Nganhangungvien.com có quyền xóa bỏ thông tin tuyển dụng, khóa tài khoản bất cứ lúc nào mà không phải báo trước hoặc bồi thường đối với các thông tin do người sử dụng cung cấp nếu thông tin đó vi phạm một trong các điều khoản sử dụng website, không trung thực, vi phạm pháp luật, không phù hợp với thuần phong mỹ tục, đạo đức xã hội, ảnh hưởng xấu đến dư luận xã hội, vi phạm hoặc không tuân thủ các qui định, thỏa thuận, hướng dẫn trên website này, hoặc theo yêu cầu của cơ quan quản lý nhà nước có thẩm quyền.</strong></p>
+
+                <p>• <strong>Khi bạn vi phạm một trong các điều nêu trên Nganhangungvien.com có quyền khóa tài khoản tạm thời hoặc khóa tài khoản vĩnh viễn mà không hoàn trả ngày sử dụng và chi phí đã thanh toán.</strong><br>
+                <br>
+                •&nbsp;&nbsp;&nbsp; Nganhangungvien.com khẳng định rằng nội dung qui định truy cập Nganhangungvien.com có thể thay đổi bất cứ lúc nào tùy theo sự thay đổi của Pháp Luật và hoàn cảnh Xã Hội.</p>
+
+                <p>Điều khoản kiểm duyệt tin là một phần không thể tách rời của bản thỏa thuận này: <a href="/dieu-khoan-su-dung" target="_blank"></a><br>
+                <br>
+                Bản thỏa thuận này sẽ được quản lý và thực thi theo Quy Định Pháp Luật của Nước Cộng Hòa Xã Hội Chủ Nghĩa Việt Nam. Bản thỏa thuận này dành riêng cho bạn và Nganhangungvien.com, bạn không thể chuyển quyền và nghĩa vụ của bạn cho bất kỳ ai . Khi bạn truy cập và sử dụng mọi tiện ích và dịch vụ từ Nganhangungvien.com mặc nhiên xem như bạn đã đồng ý và ràng buộc với điều khoản sử dụng này.</p>                            
+
+        @endif
+    </div>
+</div>
+
+<div class="policy-checkbox">
+    <label>
+        <input type="checkbox" required data-required-msg="Vui lòng chọn đồng ý với điều khoản sử dụng"> 
+        Tôi đồng ý với điều khoản sử dụng </label>
+</div>
