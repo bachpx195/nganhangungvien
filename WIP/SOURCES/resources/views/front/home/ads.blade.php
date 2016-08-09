8132b83f9f7aab77ff9d5816314a559386996a2e
THANH LE VAN <thanhlv@bloomgoo.vn>
2016-08-09 08:57:56 +0700

    Update link for banner

--- WIP/SOURCES/resources/views/front/home/ads.blade.php
+++ WIP/SOURCES/resources/views/front/home/ads.blade.php
@@ -1,7 +1,7 @@
 <div class="top-ads clearfix mb4">
 	<div class="row w_100 txc">
-		<img class="mr_30" style="height: 91px; width: 469px;" src="{{ !empty($bannerLeftImageConfig['value']) ? URL::to('/') . '/banner/images/' . $bannerLeftImageConfig->value : URL::asset('assets/default/images/banner.png') }}">
-		<img class="mr_30" style="height: 91px; width: 469px;" src="{{ !empty($bannerRightImageConfig['value']) ? URL::to('/') . '/banner/images/' . $bannerRightImageConfig->value : URL::asset('assets/default/images/banner.png') }}">
+		<a href="{{route('employer.register') }}"><img class="mr_30" style="height: 91px; width: 469px;" src="{{ !empty($bannerLeftImageConfig['value']) ? URL::to('/') . '/banner/images/' . $bannerLeftImageConfig->value : URL::asset('assets/default/images/banner.png') }}"></a>
+		<a href="{{route('candidate/form') }}"><img class="mr_30" style="height: 91px; width: 469px;" src="{{ !empty($bannerRightImageConfig['value']) ? URL::to('/') . '/banner/images/' . $bannerRightImageConfig->value : URL::asset('assets/default/images/banner.png') }}"></a>
 
 	</div>
 </div>
